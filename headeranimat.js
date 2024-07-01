cbf2906ce00e07ca04cf6e3d8d50034d42ae9e65
Eui Jin Lee <Dowha132@yonsei.ac.kr>
2024-07-02 00:26:55 +0900

    allmerge1

--- headeranimat.js
+++ headeranimat.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function() {
     var header = document.querySelector('.header');
     if (window.scrollY > 100) {
-        header.style.backgroundColor = 'rgba(61, 77, 106, 0.8)';
+        header.style.backgroundColor = 'rgba(40, 58, 90, 0.9)';
     } else {
         header.style.backgroundColor = 'rgba(55, 81, 126, 0.9)';
     }
